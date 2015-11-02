1392b964e25cee58f745ab63d81aea3536234c33
Pawel Muraczewski <pawel.muraczewski@gmail.com>
2015-11-02 19:06:18 +0100

    Create a solution

---
+++ Solution/OptimizationAlgorithms/IAlgorithm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptimizationAlgorithms
+{
+    public interface IAlgorithm
+    {
+        IEnumerable<int> RunAlgorithm(IEnumerable<int> inputPermutation);
+    }
+}
---
+++ Solution/OptimizationAlgorithmsApplication/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OptimizationAlgorithms;
+
+namespace OptimizationAlgorithmsApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var algorithm = new AntColony();
+            var inputPermutation = new List<int> { 1, 2, 3 };
+
+            algorithm.RunAlgorithm(inputPermutation);
+        }
+    }
+}
